33886be2cfc99f3ac7110af2bf863a221b9f8135
cmcgowan2 <33874173+cmcgowan2@users.noreply.github.com>
2017-12-05 15:12:33 +0000

    js editing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-  $(.chemlink).hover(function(){
-    $(.changer).attr("src", "https://cdn.pixabay.com/photo/2017/04/16/18/08/test-tube-2235388__340.png");
+$(document).ready(function() {
+  $(.chemlink).hover(function() {
+    $(.change).attr("src", "https://cdn.pixabay.com/photo/2017/04/16/18/08/test-tube-2235388__340.png");
   }, function() {
-  });
+  }); 
 });
